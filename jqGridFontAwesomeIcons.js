2c63c278b5451f7cf470810b6dbc917469ead86e
Guy Lando <glando1991@gmail.com>
2015-10-01 20:12:50 +0300

    Create jqGridFontAwesomeIcons.js

---
+++ jqGridFontAwesomeIcons.js
@@ -0,0 +1,50 @@
+/* 
+  Changes Bootstrap styleUI icons to Font-Awesome icons.
+  Call this function before initializing the grid.
+*/
+function setjqGridFontAwesomeIcons() {
+    $.jgrid.styleUI.Bootstrap.common.icon_base = "fa";
+    $.jgrid.styleUI.Bootstrap.base.icon_first = "fa-angle-double-left";
+    $.jgrid.styleUI.Bootstrap.base.icon_prev = "fa-angle-left";
+    $.jgrid.styleUI.Bootstrap.base.icon_next = "fa-angle-right";
+    $.jgrid.styleUI.Bootstrap.base.icon_end = "fa-angle-double-right";
+    $.jgrid.styleUI.Bootstrap.base.icon_asc = "fa-sort-asc";
+    $.jgrid.styleUI.Bootstrap.base.icon_desc = "fa-sort-desc";
+    $.jgrid.styleUI.Bootstrap.base.icon_caption_open = "fa-arrow-circle-up";
+    $.jgrid.styleUI.Bootstrap.base.icon_caption_close = "fa-arrow-circle-down";
+    $.jgrid.styleUI.Bootstrap.modal.icon_close = "fa-times-circle";
+    $.jgrid.styleUI.Bootstrap.modal.icon_resizable = "fa-signal";
+    $.jgrid.styleUI.Bootstrap.inlinedit.icon_edit_nav = "fa-pencil-square";
+    $.jgrid.styleUI.Bootstrap.inlinedit.icon_add_nav = "fa-plus-square";
+    $.jgrid.styleUI.Bootstrap.inlinedit.icon_save_nav = "fa-floppy-o";
+    $.jgrid.styleUI.Bootstrap.inlinedit.icon_cancel_nav = "fa-times-circle-o";
+    $.jgrid.styleUI.Bootstrap.formedit.icon_prev = "fa-angle-left";
+    $.jgrid.styleUI.Bootstrap.formedit.icon_next = "fa-angle-right";
+    $.jgrid.styleUI.Bootstrap.formedit.icon_save = "fa-floppy-o";
+    $.jgrid.styleUI.Bootstrap.formedit.icon_close = "fa-times-circle";
+    $.jgrid.styleUI.Bootstrap.formedit.icon_del = "fa-trash";
+    $.jgrid.styleUI.Bootstrap.formedit.icon_cancel = "fa-times-circle-o";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_edit_nav = "fa-pencil-square-o";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_add_nav = "fa-plus-square-o";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_del_nav = "fa-trash-o";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_search_nav = "fa-search";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_refresh_nav = "fa-refresh";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_view_nav = "fa-info-circle";
+    $.jgrid.styleUI.Bootstrap.navigator.icon_newbutton_nav = "fa-external-link";
+    $.jgrid.styleUI.Bootstrap.grouping.icon_plus = 'fa-plus';
+    $.jgrid.styleUI.Bootstrap.grouping.icon_minus = 'fa-minus';
+    $.jgrid.styleUI.Bootstrap.filter.icon_search = 'fa-search';
+    $.jgrid.styleUI.Bootstrap.filter.icon_reset = 'fa-refresh';
+    $.jgrid.styleUI.Bootstrap.filter.icon_query = 'fa-comment';
+    $.jgrid.styleUI.Bootstrap.subgrid.icon_plus = 'fa-plus';
+    $.jgrid.styleUI.Bootstrap.subgrid.icon_minus = 'fa-minus';
+    $.jgrid.styleUI.Bootstrap.subgrid.icon_open = 'fa-search-plus';
+    $.jgrid.styleUI.Bootstrap.treegrid.icon_plus = 'fa-plus';
+    $.jgrid.styleUI.Bootstrap.treegrid.icon_minus = 'fa-minus';
+    $.jgrid.styleUI.Bootstrap.treegrid.icon_leaf = 'fa-leaf';
+    $.jgrid.styleUI.Bootstrap.fmatter.icon_edit = "fa-pencil-square-o";
+    $.jgrid.styleUI.Bootstrap.fmatter.icon_add = "fa-plus-square-o";
+    $.jgrid.styleUI.Bootstrap.fmatter.icon_save = "fa-floppy-o";
+    $.jgrid.styleUI.Bootstrap.fmatter.icon_cancel = "fa-times-circle-o";
+    $.jgrid.styleUI.Bootstrap.fmatter.icon_del = "fa-trash-o";
+}
